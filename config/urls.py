17f3b195d6339b4e46ffc32c23a6007bd0419a64
chykB <malikchika86@gmail.com>
2026-01-23 11:33:10 +0100

    Added analytics app into settings and include the app into the based urls

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/reports/", include("reports.urls"))
+    path("api/reports/", include("reports.urls")),
+    path("api/analytics/", include("analytics.urls")),
 ]
